833ad9380e46a192cea5454c6dbe6fc5742c0e81
Abdullah Althobetey <aalthobetey@elm.sa>
2017-10-11 11:33:10 +0300

    Add the CoreDataStack.swift file by Udacity

---
+++ VirtualTourist/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Abdullah Althobetey on 10/11/17.
+//  Copyright © 2017 Abdullah Althobetey. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+}
+
